05624d5d2b0c7ef63a26b25faccf491882ac8a13
Ameliance SkyMusic <amelianceskymusic@gmail.com>
2022-01-19 23:58:04 +0200

    feat: add interactive js

--- portfolio/index.js
+++ portfolio/index.js
@@ -1,2 +1,23 @@
+/* jshint esversion: 6 */
+// /*jshint -W033 */
+const icon = document.querySelector('.header__float-menu');
+const iconClose = document.querySelector('.header__float-menu-close');
+const nav = document.querySelector('.nav');
 
-console.log("Вёрстка валидная +10\nВёрстка семантическая +20\nВёрстка соответствует макету +48\nТребования к css + 12\nИнтерактивность, реализуемая через css +20\nИтого: 110")
+function addOpenClassToMenu() {
+    nav.classList.toggle('open');
+}
+
+function closeMenu(event){
+    if (event.target.classList.contains('menu__link')) {
+        nav.classList.remove('open');
+    }
+}
+
+icon.addEventListener('click', addOpenClassToMenu);
+iconClose.addEventListener('click', addOpenClassToMenu);
+
+nav.addEventListener('click', closeMenu);
+
+
+// console.log("Вёрстка валидная +10\nВёрстка семантическая +20\nВёрстка соответствует макету +48\nТребования к css + 12\nИнтерактивность, реализуемая через css +20\nИтого: 110")
