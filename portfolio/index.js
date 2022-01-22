041ab813a376d7cdb526760a22d237ae25709430
Ameliance SkyMusic <amelianceskymusic@gmail.com>
2022-01-22 14:56:13 +0200

    fix: adaptive menu

--- portfolio/index.js
+++ portfolio/index.js
@@ -1,23 +1,16 @@
 /* jshint esversion: 6 */
 // /*jshint -W033 */
-const icon = document.querySelector('.header__float-menu');
-const iconClose = document.querySelector('.header__float-menu-close');
+const icon = document.querySelector('.hamburger-menu');
 const nav = document.querySelector('.nav');
+const blackout = document.querySelector('.blackout');
 
-function addOpenClassToMenu() {
+function addOpenClasstoMenu() {
+    icon.classList.toggle('open');
     nav.classList.toggle('open');
+    blackout.classList.toggle('open');
 }
 
-function closeMenu(event){
-    if (event.target.classList.contains('menu__link')) {
-        nav.classList.remove('open');
-    }
-}
-
-icon.addEventListener('click', addOpenClassToMenu);
-iconClose.addEventListener('click', addOpenClassToMenu);
-
-nav.addEventListener('click', closeMenu);
-
+icon.addEventListener('click', addOpenClasstoMenu);
+nav.addEventListener('click', addOpenClasstoMenu);
 
 // console.log("Вёрстка валидная +10\nВёрстка семантическая +20\nВёрстка соответствует макету +48\nТребования к css + 12\nИнтерактивность, реализуемая через css +20\nИтого: 110")
