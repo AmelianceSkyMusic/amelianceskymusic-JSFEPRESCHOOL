3f09d3de6591247c9eb2b698e363457a28be0f0c
Ameliance SkyMusic <amelianceskymusic@gmail.com>
2022-02-10 15:18:52 +0200

    feat: add getData function

--- movie-app/script.js
+++ movie-app/script.js
@@ -4,3 +4,18 @@
 // ^................... ( Msg from console.log ) ....................
 
 const Msg = msg => console.log(msg);
+
+// >----------------------------------------------------------------<
+// >                            GET DATA                            <
+// >----------------------------------------------------------------<
+
+// ^--------------------------- get data ----------------------------
+
+const url =  "https://api.themoviedb.org/3/discover/movie?sort_by=popularity.desc&api_key=085ded34e7ba5e71fc4264485245f7f0&language=ru-RU&include_image_language=ru,null";
+
+async function getMovieData() {
+    const res = await fetch(url);
+    const data = await res.json();
+    Msg(data.results);
+    showMovieData(data);
+}
