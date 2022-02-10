0427bcb7b988d338b608e9338c5303ac52752bd7
Ameliance SkyMusic <amelianceskymusic@gmail.com>
2022-02-10 15:19:46 +0200

    feat: add showMovieData function

--- movie-app/script.js
+++ movie-app/script.js
@@ -19,3 +19,69 @@ async function getMovieData() {
     Msg(data.results);
     showMovieData(data);
 }
+
+const showMovieData = (data) => {
+
+    const createDiv = (divClass, parentName) => {
+        const divName = document.createElement('div');
+        divName.classList.add(divClass);
+        parentName.append(divName);
+        return divName;
+    };
+
+    const moviesContainer = document.querySelector('.movies-container'); // container for all cards
+
+    let result = data.results.map((key) => {
+
+
+        // const wrapperCardDecor = document.createElement('div');
+        // wrapperCardDecor.classList.add('wrapper-card-decor');
+        // moviesContainer.append(wrapperCardDecor);
+
+        const wrapperCardDecor = createDiv('wrapper-card-decor', moviesContainer);
+
+            const moviesContainerCard = createDiv('movie-card', wrapperCardDecor);
+
+            // const moviesContainerCard = document.createElement('div');
+            // moviesContainerCard.classList.add('movie-card');
+            // wrapperCardDecor.append(moviesContainerCard);
+
+                const image = document.createElement('img');
+                image.classList.add('movie-card__image');
+                image.src = `https://image.tmdb.org/t/p/w500${key.poster_path}`;
+                image.alt = key.title;
+                moviesContainerCard.append(image);
+
+                const title = document.createElement('p');
+                title.classList.add('movie-card__title');
+                title.innerText = key.title;
+                moviesContainerCard.append(title);
+
+                // const date = document.createElement('p');
+                // date.classList.add('movie-card__date');
+                // const splitDate = key.release_date.split("-");
+                // date.innerText = splitDate[0];
+                // moviesContainerCard.append(date);
+
+                // const moviesCardVoteWrapper = document.createElement('div');
+                // moviesCardVoteWrapper.classList.add('movie-card__vote-wrapper');
+                // moviesContainerCard.append(moviesCardVoteWrapper);
+
+                const moviesCardVoteWrapper = createDiv('movie-card__vote-wrapper', moviesContainerCard);
+
+                    const voteIcon = document.createElement('div');
+                    voteIcon.classList.add('movie-card__vote-icon');
+                    voteIcon.style.backgroundImage = 'url("./assets/svg/vote.svg")';
+                    moviesCardVoteWrapper.append(voteIcon);
+
+                    const vote = document.createElement('p');
+                    vote.classList.add('movie-card__vote');
+                    vote.innerText = key.vote_average;
+                    moviesCardVoteWrapper.append(vote);
+
+
+
+    });
+};
+
+getMovieData();
